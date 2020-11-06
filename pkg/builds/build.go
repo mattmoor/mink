ebc1eebdcd5c1f59366db7d55c7b1c166a6d2c26
Matt Moore <mattmoor@vmware.com>
2020-11-06 15:41:32 -0800

    Attach imagePullSecrets

--- pkg/builds/build.go
+++ pkg/builds/build.go
@@ -206,6 +206,10 @@ func WithServiceAccount(sa string, refs ...name.Reference) CancelableOption {
 				GenerateName: tr.GenerateName,
 				Namespace:    tr.Namespace,
 			},
+			// Support pulling kontext using the user credentials.
+			ImagePullSecrets: []corev1.LocalObjectReference{{
+				Name: secret.Name,
+			}},
 		}
 		sa, err = client.CoreV1().ServiceAccounts(sa.Namespace).Create(ctx, sa, metav1.CreateOptions{})
 		if err != nil {
