9c03b9cd9a985c6f86b10c81bae39ad3c2870959
James Strachan <james.strachan@gmail.com>
2020-11-21 07:16:32 +0000

    fix: add an optional output directory for resolve
    so that the yaml files are written to a directory and output the build logs
    
    this lets you modify the yaml files in place on disk or output to a separate folder so that the yaml can then be processed on the file system as separate files.
    
    this also lets us modify source code in a release pipeline to create new YAML to release (e.g. to publish or include in helm charts or whatever)

--- pkg/command/resolve.go
+++ pkg/command/resolve.go
@@ -37,6 +37,7 @@ import (
 	"github.com/mattmoor/mink/pkg/builds/dockerfile"
 	"github.com/mattmoor/mink/pkg/builds/ko"
 	"github.com/mattmoor/mink/pkg/source"
+	errs "github.com/pkg/errors"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 	"github.com/tektoncd/cli/pkg/cli"
@@ -100,7 +101,13 @@ type ResolveOptions struct {
 
 	Parallelism int
 
+	// OutputDir specifies the directory to write the resolved yaml files to.
+	// If the current directory then the files will be overwritten with the resolved values
+	// Use a separate directory to avoid modifying the source
+	OutputDir string
+
 	builders map[string]builder
+	cmd      *cobra.Command
 }
 
 // ResolveOptions implements Interface
@@ -119,6 +126,7 @@ func (opts *ResolveOptions) AddFlags(cmd *cobra.Command) {
 	cmd.Flags().BoolP("recursive", "R", false,
 		"Process the directory used in -f, --filename recursively. Useful when you want to manage related manifests organized within the same directory.")
 	cmd.Flags().IntP("parallelism", "P", 20, "How many parallel builds to run at once.")
+	cmd.Flags().StringP("output", "O", "", "Output directory for resolved YAML files to be written. If the current directory then the YAML files are resolved and modified in place. Otherwise the files are written to the output directory using relative paths from the current directory.")
 }
 
 // Validate implements Interface
@@ -145,12 +153,15 @@ func (opts *ResolveOptions) Validate(cmd *cobra.Command, args []string) error {
 		return apis.ErrInvalidValue(opts.Parallelism, "parallelism")
 	}
 
+	opts.OutputDir = viper.GetString("output")
+
 	opts.builders = map[string]builder{
 		"dockerfile": opts.db,
 		"buildpack":  opts.bp,
 		"ko":         opts.ko,
 	}
 
+	opts.cmd = cmd
 	return nil
 }
 
@@ -175,30 +186,73 @@ func (opts *ResolveOptions) execute(ctx context.Context, cmd *cobra.Command) err
 
 	// Turn the files into yaml nodes.
 	files := opts.EnumerateFiles()
-	blocks := make([]*yaml.Node, 0, len(files))
+	fileBlocks := make([]*yamlDoc, 0, len(files))
 	for _, f := range files {
 		bs, err := opts.ResolveFile(ctx, f)
 		if err != nil {
 			return err
 		}
-		blocks = append(blocks, bs...)
+		fileBlocks = append(fileBlocks, &yamlDoc{path: f, blocks: bs})
 	}
 
 	// Turn all of the images references in the yaml nodes into digests.
-	if err := opts.ResolveReferences(ctx, blocks, sourceSteps, nameRefs); err != nil {
+	if err := opts.ResolveReferences(ctx, fileBlocks, sourceSteps, nameRefs); err != nil {
 		return err
 	}
 
 	// Encode the resulting yaml
-	e := yaml.NewEncoder(cmd.OutOrStdout())
-	e.SetIndent(2)
-	for _, doc := range blocks {
-		if err := e.Encode(doc); err != nil {
-			return fmt.Errorf("failed to encode output: %w", err)
+	for _, fb := range fileBlocks {
+		out, err := opts.writeForFile(fb.path)
+		if err != nil {
+			return err
 		}
+		var e *yaml.Encoder
+		if out != nil {
+			defer out.Close()
+			e = yaml.NewEncoder(out)
+		} else {
+			e = yaml.NewEncoder(cmd.OutOrStdout())
+		}
+		e.SetIndent(2)
+		for _, doc := range fb.blocks {
+			if err := e.Encode(doc); err != nil {
+				return fmt.Errorf("failed to encode output: %w", err)
+			}
+		}
+		e.Close()
 	}
-	e.Close()
 	return nil
+}
+
+func (opts *ResolveOptions) writeForFile(path string) (io.WriteCloser, error) {
+	if opts.OutputDir != "" {
+		dir, err := os.Getwd()
+		if err != nil {
+			return nil, errs.Wrapf(err, "failed to get current directory")
+		}
+		absPath, err := filepath.Abs(path)
+		if err != nil {
+			return nil, errs.Wrapf(err, "failed to find absolute path for %s", path)
+		}
+		rel, err := filepath.Rel(dir, absPath)
+		if err != nil {
+			return nil, errs.Wrapf(err, "failed to find relative path for %s", absPath)
+		}
+		f := filepath.Join(opts.OutputDir, rel)
+		d := filepath.Dir(f)
+		err = os.MkdirAll(d, 0760)
+		if err != nil {
+			return nil, errs.Wrapf(err, "failed to create output directory %s", d)
+		}
+		log.Printf("writing file %s\n", f)
+		return os.Create(f)
+	}
+	return nil, nil
+}
+
+type yamlDoc struct {
+	path   string
+	blocks []*yaml.Node
 }
 
 // EnumerateFiles is based heavily on pkg/kubectl
@@ -282,16 +336,18 @@ func (opts *ResolveOptions) ResolveFile(ctx context.Context, f string) (blocks [
 }
 
 // ResolveReferences is based heavily on ko's ImageReferences
-func (opts *ResolveOptions) ResolveReferences(ctx context.Context, docs []*yaml.Node, sourceSteps []tknv1beta1.Step, nameRefs []name.Reference) error {
+func (opts *ResolveOptions) ResolveReferences(ctx context.Context, fileBlocks []*yamlDoc, sourceSteps []tknv1beta1.Step, nameRefs []name.Reference) error {
 	// First, walk the input objects and collect a list of supported references
 	refs := make(map[string][]*yaml.Node)
 
-	for _, doc := range docs {
-		it := opts.refsFromDoc(doc)
+	for _, fb := range fileBlocks {
+		for _, doc := range fb.blocks {
+			it := opts.refsFromDoc(doc)
 
-		for node, ok := it(); ok; node, ok = it() {
-			ref := strings.TrimSpace(node.Value)
-			refs[ref] = append(refs[ref], node)
+			for node, ok := it(); ok; node, ok = it() {
+				ref := strings.TrimSpace(node.Value)
+				refs[ref] = append(refs[ref], node)
+			}
 		}
 	}
 
@@ -354,25 +410,35 @@ func (opts *ResolveOptions) db(ctx context.Context, sourceSteps []tknv1beta1.Ste
 
 	tr := dockerfile.Build(ctx, sourceSteps, opts.tag, dockerfile.Options{
 		Dockerfile: filepath.Join(u.Path, opts.Dockerfile),
+		KanikoArgs: opts.KanikoArgs,
 	})
 	tr.Namespace = Namespace()
 
 	// Buffer the output, so we can display it on failures.
-	buf := &bytes.Buffer{}
+	var buf *bytes.Buffer
+	var out io.Writer
+	if opts.OutputDir == "" {
+		buf = &bytes.Buffer{}
+		// lets only log output on errors so we can pipe the output to kubectl etc
+		out = buf
+	} else {
+		out = opts.cmd.OutOrStderr()
+	}
 
 	// Run the produced Build definition to completion, streaming logs to stdout, and
 	// returning the digest of the produced image.
 	digest, err := builds.Run(ctx, opts.ImageName, tr, &options.LogOptions{
 		Params: &cli.TektonParams{},
 		Stream: &cli.Stream{
-			// Send Out to stderr so we can capture the digest for composition.
-			Out: buf,
-			Err: buf,
+			Out: out,
+			Err: out,
 		},
 		Follow: true,
 	}, builds.WithServiceAccount(opts.ServiceAccount, nameRefs...))
 	if err != nil {
-		log.Print(buf.String())
+		if buf != nil {
+			log.Print(buf.String())
+		}
 		return name.Digest{}, err
 	}
 	return digest, nil
